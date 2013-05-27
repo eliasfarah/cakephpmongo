665916310dec984e29eb9a6150f0c159b8cddfe7
Elias Farah <eliasfa@gmail.com>
2013-05-27 17:16:07 -0300

    Modificando security salt e outros dados

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'kjffhiuefhiu*#*ˆ#*(FHSJSgkfgIUWGFIGF*#*&#$&@)()');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '84374362094720492402962626262010109');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
